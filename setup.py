4c8b823ba6a263b9ca57773054ee186e9943dc03
Robert Siemer <Robert.Siemer-git@backsla.sh>
2016-08-13 03:19:36 +0200

    add setup.py with basics

---
+++ setup.py
@@ -0,0 +1,14 @@
+
+
+from setuptools import setup
+
+setup(
+    name='athemala',
+    version='0.1.0',
+    description='A HTML generation system',
+    author='Robert Siemer',
+    author_email='Robert.Siemer-pypi@backsla.sh',
+    license='AGPL3',
+    url='https://github.com/siemer/athemala',
+    py_modules=['athemala'],
+)
